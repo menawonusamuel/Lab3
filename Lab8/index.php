9b6998baa2d1736b47c96ab5fe0a9f987eddce95
menawonusamuel <1813617@rgu.ac.uk>
2019-02-25 18:08:17 +0000

    newly edited

--- Lab8/index.php
+++ Lab8/index.php
@@ -14,9 +14,9 @@
      <ul>
          <li><a href="index.php">Home</a> </li>
          <li><a href="all_movies.php">All movies</a> </li>
-         <li><a href="mervel-movies.php">Mervel Studios Movies</a> </li>
+         <li><a href="marvel-movies.php">Marvel Studios Movies</a> </li>
          <li><a href="after2010.php">Movies After 2010</a> </li>
-         <li><a href="xmen-films.php">X-men Firms</a> </li>
+         <li><a href="xmen-films.php">X-men Films</a> </li>
 
 
      </ul>
